be0c20917ac49ca3fc6ff4dbd05c117037cd6336
imran-shah-codes <monishah121413@gmail.com>
2025-11-03 23:19:07 -0800

    search_string.py
    user enter a number and checks if that particular number is on the list or not

---
+++ 2_nov_2025/program search a string.py
@@ -0,0 +1,6 @@
+numbers = [1,5,9,34,32,7,12,21,23]
+num = int(input("Enter a number: "))
+if num in numbers:
+    print("Yes! The number is on the list.")
+else:
+    print("No! The number is not in the list.")
