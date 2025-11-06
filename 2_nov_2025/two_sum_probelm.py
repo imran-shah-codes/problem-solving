840c4a284e1645ff87d2c5f7c85f0ab1fc7bdb30
imran-shah-codes <monishah121413@gmail.com>
2025-11-06 02:57:19 -0800

    two_sum_probelm.py

---
+++ 2_nov_2025/two_sum_probelm.py
@@ -0,0 +1,11 @@
+def twosum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i + 1,len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i,j]
+            return[]
+        
+nums = [2,7,11,15]
+target = 9
+result = twosum(nums, target)
+print("Indicates of the numbers that add uo to target:",result)
